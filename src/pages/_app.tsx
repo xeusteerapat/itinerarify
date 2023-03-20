8637baed857eacfe8690a7671aed54f815601ee0
xeusteerapat <xeus085@gmail.com>
2023-03-20 21:27:48 +0700

    :sparkles: [Update] apply mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,37 @@
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
+import Head from 'next/head';
+import { type AppType } from 'next/app';
+import { type Session } from 'next-auth';
+import { MantineProvider } from '@mantine/core';
+import { SessionProvider } from 'next-auth/react';
 
-import { api } from "~/utils/api";
-
-import "~/styles/globals.css";
+import { api } from '~/utils/api';
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width'
+        />
+      </Head>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'light',
+        }}
+      >
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </MantineProvider>
+    </>
   );
 };
 
